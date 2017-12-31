458f5a2d920877a81c981ca8bd2a4a0285321a49
Atynine <rylanhoss@gmail.com>
2017-12-31 16:16:07 -0600

    Remove wingpanel-indicator-sound

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -15,3 +15,9 @@ echo "~~~~~~~~~~"
 echo "Removing guest account"
 echo "~~~~~~~~~~"
 sudo cp files/40-pantheon-greeter.conf /usr/share/lightdm/lightdm.conf.d
+
+#Remove wingpanel-indicator-sound(Crashes wingpanel when spotify opens)
+echo "~~~~~~~~~~"
+echo "Removing wingpanel-indicator-sound"
+echo "~~~~~~~~~~"
+sudo apt-get remove wingpanel-indicator-sound
