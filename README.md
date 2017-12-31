06d0b708548775ad4bf03cde2f6026c1289a6150
Atynine <rylanhoss@gmail.com>
2017-12-30 23:25:48 -0600

    Remove Guest Account

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 Sets Elementary OS preferences and installs software for an Acer C720
 
 ## Preferences
+* Removes guest account
 * Changes trackpad sensitivity
 * Rebinds the top row from F-keys to media keys
 
--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -1,7 +1,17 @@
 #Remove unwanted apps that come installed with Elementary
+echo "~~~~~~~~~~"
+echo "Removing default apps"
+echo "~~~~~~~~~~"
 sudo apt-get -y remove epiphany-browser epiphany-browser-data \
 gnome-icon-theme-symbolic libavahi-gobject0 pantheon-calculator \
 audience pantheon-mail pantheon-photos maya-calendar* noise* \
 scratch-text-editor screenshot-tool pantheon-print snap-photobooth \
 simple-scan appcenter
 sudo apt-get -y autoremove
+
+
+#Remove guest account
+echo "~~~~~~~~~~"
+echo "Removing guest account"
+echo "~~~~~~~~~~"
+sudo cp files/40-pantheon-greeter.conf /usr/share/lightdm/lightdm.conf.d
