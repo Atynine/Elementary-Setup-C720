fd7bed7283c3c8d34b716c5f635e93ec5939e192
Rylan Hoss <RylanHoss@gmail.com>
2018-01-01 16:23:52 -0600

    Backup keyboard shortcuts

--- scripts/keyboard.sh
+++ scripts/keyboard.sh
@@ -1,5 +1,7 @@
 sudo cp files/pc /usr/share/X11/xkb/symbols/
 sudo rm -rf /var/lib/xkb/*
+sudo dconf load /org/gnome/desktop/wm/keybindings/ < files/keybindings
+sudo dconf load /org/gnome/settings-daemon/plugins/media-keys/ < files/media-keys
 echo "~~~~~~~~~~"
-echo "Remapped media keys"
+echo "Remapped keyboard"
 echo "~~~~~~~~~~"
